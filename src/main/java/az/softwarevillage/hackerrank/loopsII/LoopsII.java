269cc67f713a280abb68ecdff1ea4ea25fdd40f8
HidayaddinPashayev <hpashayev18207@ada.edu.az>
2024-12-23 21:32:43 +0400

    Loops II is  added

---
+++ src/main/java/az/softwarevillage/hackerrank/loopsII/LoopsII.java
@@ -0,0 +1,28 @@
+package az.softwarevillage.hackerrank.loopsII;
+import java.util.Scanner;
+
+public class LoopsII {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int sum = a;
+
+        for(int j=0;j<n;j++){
+            sum += (int) (Math.pow(2,j)*b) ;
+            System.out.print(sum+ " ");
+
+
+        }
+            System.out.println();
+
+        }
+
+
+
+    }
+}
